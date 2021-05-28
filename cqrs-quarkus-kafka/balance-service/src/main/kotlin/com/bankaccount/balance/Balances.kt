0bc00a40d5c3ddc3d166a5aac17c826419c53eef
Adilson de Brito <abrito10@gmail.com>
2021-05-28 11:41:42 -0300

    primeiro commit

---
+++ cqrs-quarkus-kafka/balance-service/src/main/kotlin/com/bankaccount/balance/Balances.kt
@@ -0,0 +1,11 @@
+package com.bankaccount.balance
+
+interface Balances {
+
+    fun saveOrUpdate(balance: Balance): Balance
+
+    fun findByAccountId(accountId: String): Balance?
+
+    fun recalculate(transaction: Transaction): Balance
+
+}
